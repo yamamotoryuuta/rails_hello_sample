f46fe27f99a1d71a8674df0865170d3d15712eee
山本龍太 <yamamotoryuuta@yamamotoryuutanoiMac.local>
2020-03-12 09:21:51 +0900

    task_3_12_01

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+  def index
+    @message = "Hello World!!!!!"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  
+    get '/users', to: 'users#index'
+
+end
